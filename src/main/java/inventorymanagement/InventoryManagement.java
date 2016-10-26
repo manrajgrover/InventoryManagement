aac92016652bc3325808f3db5c1ec9e7ba92f3d6
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-26 17:22:55 +0530

    Fix: Adds Tomcat Context to Initializer

--- src/main/java/inventorymanagement/InventoryManagement.java
+++ src/main/java/inventorymanagement/InventoryManagement.java
@@ -2,9 +2,16 @@ package inventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InventoryManagement {
+public class InventoryManagement extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InventoryManagement.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagement.class, args);
