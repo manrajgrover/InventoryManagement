be0b70038d38855444a22d834c8b2488fcd1753a
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-26 18:11:11 +0530

    Fix: Script updated in dist/

--- src/main/resources/static/dist/js/script.js
+++ src/main/resources/static/dist/js/script.js
@@ -49941,31 +49941,31 @@ app.config(function($httpProvider) {
 app.config(function($routeProvider) {
   $routeProvider.when('/', {
     controller: 'homeController',
-    templateUrl: '/dist/views/home.html'
+    templateUrl: 'dist/views/home.html'
   }).when('/dashboard', {
     controller: 'dashboardController',
-    templateUrl: '/dist/views/dashboard.html'
+    templateUrl: 'dist/views/dashboard.html'
   }).when('/profile', {
     controller: 'profileController',
-    templateUrl: '/dist/views/profile.html'
+    templateUrl: 'dist/views/profile.html'
   }).when('/admin', {
     controller: 'adminController',
-    templateUrl: '/dist/views/admin.html'
+    templateUrl: 'dist/views/admin.html'
   }).when('/selectRole', {
     controller: 'selectRoleController',
-    templateUrl: '/dist/views/selectRole.html'
+    templateUrl: 'dist/views/selectRole.html'
   }).when('/incoming/requests', {
     controller: 'incomingRequestsController',
-    templateUrl: '/dist/views/incomingRequests.html'
+    templateUrl: 'dist/views/incomingRequests.html'
   }).when('/products', {
     controller: 'productController',
-    templateUrl: '/dist/views/product.html'
+    templateUrl: 'dist/views/product.html'
   }).when('/items', {
     controller: 'itemController',
-    templateUrl: '/dist/views/item.html'
+    templateUrl: 'dist/views/item.html'
   }).when('/returns', {
     controller: 'returnsController',
-    templateUrl: '/dist/views/returns.html'
+    templateUrl: 'dist/views/returns.html'
   }).otherwise({
     redirectTo: '/'
   });
@@ -49992,10 +49992,10 @@ app.run(function($rootScope, $location, sessionService){
   $rootScope.$on( "$routeChangeStart", function(event, next, current) {
     let session = sessionService.getSession();
     if (!session.hasOwnProperty('authenticated') || session.authenticated === false) {
-      if ( next.templateUrl !== "/dist/views/home.html" ) {
+      if ( next.templateUrl !== "dist/views/home.html" ) {
         $location.path( "/" );
       }
-    } else if ((!session.hasOwnProperty('admin') || session.admin === false) && (next.templateUrl === "/dist/views/admin.html" || next.templateUrl === "/dist/views/selectRole.html")) {
+    } else if ((!session.hasOwnProperty('admin') || session.admin === false) && (next.templateUrl === "dist/views/admin.html" || next.templateUrl === "dist/views/selectRole.html")) {
       $location.path( "/dashboard" );
     }
   });
@@ -50033,7 +50033,7 @@ app.controller("navController", function($scope, $http, $location, sessionServic
     jQuery(".dropdown-toggle").dropdown();
   });
 
-  $http.get("/userLogin").success(function(data) {
+  $http.get("userLogin").success(function(data) {
     console.log(data);
 
     let session = {
@@ -50060,7 +50060,7 @@ app.controller("navController", function($scope, $http, $location, sessionServic
   });
 
   $scope.logout = function() {
-    $http.post('/logout', {}).success(function() {
+    $http.post('logout', {}).success(function() {
       sessionService.resetSession();
       $scope.session = {};
       $location.path("/");
@@ -50084,7 +50084,7 @@ const getProductInformation = ($http, $scope, id) => {
     return;
   }
 
-  $http.get(`/items/${id}/count`).success(function(data) {
+  $http.get(`items/${id}/count`).success(function(data) {
     $scope.metaInfo.visible = true;
     $scope.metaInfo.count = data;
     $scope.message = "";
@@ -50100,7 +50100,7 @@ const getProductInformation = ($http, $scope, id) => {
 
 app.controller("dashboardController", function($scope, $http, $location, $timeout, sessionService) {
 
-  $http.get("/products").success(function(data) {
+  $http.get("products").success(function(data) {
     console.log(data);
     $scope.products = data;
     $scope.metaInfo = {
@@ -50125,7 +50125,7 @@ app.controller("dashboardController", function($scope, $http, $location, $timeou
 
     let userId = $scope.session.id;
 
-    $http.post('/requests', {
+    $http.post('requests', {
       userId: userId,
       productId: productId
     }).success(function() {
@@ -50140,7 +50140,7 @@ app.controller("dashboardController", function($scope, $http, $location, $timeou
 
 app.controller("adminController", function($scope, $http, $location, $timeout, sessionService) {
 
-  $http.get("/products").success(function(data) {
+  $http.get("products").success(function(data) {
     console.log(data);
     $scope.products = data;
     $scope.metaInfo = {
@@ -50160,7 +50160,7 @@ app.controller("adminController", function($scope, $http, $location, $timeout, s
     $location.path("/");
   });
 
-  $http.get("/users").success(function(data) {
+  $http.get("users").success(function(data) {
     console.log(data);
     $scope.users = data;
 
@@ -50181,7 +50181,7 @@ app.controller("adminController", function($scope, $http, $location, $timeout, s
     let userId = jQuery("#user").val();
     let productTag = jQuery("#productTag").val();
 
-    $http.post('/history', {
+    $http.post('history', {
       userId: userId,
       productId: productId,
       productTag: productTag
@@ -50222,7 +50222,7 @@ app.controller("returnsController", function($scope, $http, $location, $timeout,
       return;
     }
 
-    $http.patch(`/history/${issueNumber}`, {
+    $http.patch(`history/${issueNumber}`, {
       productTag: productTag
     }).success(function(data) {
       console.log(data);
@@ -50249,7 +50249,7 @@ app.controller("returnsController", function($scope, $http, $location, $timeout,
 
 app.controller("incomingRequestsController", function($http, $scope, $timeout, sessionService) {
 
-  $http.get("/requests").success(function(data) {
+  $http.get("requests").success(function(data) {
     $scope.data = data;
     
   }).error(function() {
@@ -50257,7 +50257,7 @@ app.controller("incomingRequestsController", function($http, $scope, $timeout, s
   });
 
   $scope.populateModal = (id) => {
-    $http.get(`/requests/${id}`).success(function(data) {
+    $http.get(`requests/${id}`).success(function(data) {
       $scope.modalData = data;
     }).error(function() {
       console.log("Error");
@@ -50269,7 +50269,7 @@ app.controller("incomingRequestsController", function($http, $scope, $timeout, s
     $scope.updateRequest = (id) => {
       let reply = jQuery(e.currentTarget).find('textarea[name="reply"]').val();
 
-      $http.patch(`/requests/${id}`, {
+      $http.patch(`requests/${id}`, {
         reply: reply
       }).success(function(data) {
         jQuery('#httpMessage').removeClass('alert-danger');
@@ -50299,7 +50299,7 @@ app.controller("incomingRequestsController", function($http, $scope, $timeout, s
 
 app.controller("productController", function($http, $scope, $timeout, $location, $route, sessionService) {
 
-  $http.get("/products").success(function(data) {
+  $http.get("products").success(function(data) {
     console.log(data);
     $scope.data = data;
   }).error(function() {
@@ -50307,7 +50307,7 @@ app.controller("productController", function($http, $scope, $timeout, $location,
   });
 
   $scope.populateEdit = (id) => {
-    $http.get(`/products/${id}`).success(function(data) {
+    $http.get(`products/${id}`).success(function(data) {
       jQuery("#editProductCompany").val(data.company);
       jQuery("#editProductVersion").val(data.version);
       jQuery("#editProductName").val(data.name);      
@@ -50319,7 +50319,7 @@ app.controller("productController", function($http, $scope, $timeout, $location,
 
   $scope.populateDelete = (id) => {
     console.log(id);
-    $http.get(`/products/${id}`).success(function(data) { 
+    $http.get(`products/${id}`).success(function(data) { 
       console.log(data); 
       $scope.delete = data;
     }).error(function() {
@@ -50337,7 +50337,7 @@ app.controller("productController", function($http, $scope, $timeout, $location,
 
       console.log(company + " "+ name + " " + version);
 
-      $http.patch(`/products/${id}`, {
+      $http.patch(`products/${id}`, {
         name: name,
         company: company,
         version: version
@@ -50372,7 +50372,7 @@ app.controller("productController", function($http, $scope, $timeout, $location,
     $scope.deleteProduct = (id) => {
       console.log(id);
 
-      $http.delete(`/products/${id}`).success(function(data) {
+      $http.delete(`products/${id}`).success(function(data) {
         jQuery('#httpMessage').removeClass('alert-danger');
         jQuery('#httpMessage').addClass('alert-success');
         jQuery('#delete').modal('hide');
@@ -50402,7 +50402,7 @@ app.controller("productController", function($http, $scope, $timeout, $location,
 
 app.controller("itemController", function($http, $scope, $timeout, $location, $route, sessionService) {
 
-  $http.get("/items").success(function(data) {
+  $http.get("items").success(function(data) {
     console.log(data);
     $scope.data = data;
   }).error(function() {
@@ -50410,14 +50410,14 @@ app.controller("itemController", function($http, $scope, $timeout, $location, $r
   });
 
   $scope.populateEdit = (id, productId) => {
-    $http.get(`/items/${id}`).success(function(data) {  
+    $http.get(`items/${id}`).success(function(data) {  
       $scope.edit = data;
       jQuery('#editItemTag').val(data.tag);
     }).error(function() {
       console.log("Error");
     });
 
-    $http.get(`/products`).success(function(data) {  
+    $http.get(`products`).success(function(data) {  
       $scope.products = data;
       $timeout(function(){
         console.log("Selecting");
@@ -50430,7 +50430,7 @@ app.controller("itemController", function($http, $scope, $timeout, $location, $r
 
   $scope.populateDelete = (id) => {
     console.log(id);
-    $http.get(`/items/${id}`).success(function(data) { 
+    $http.get(`items/${id}`).success(function(data) { 
       console.log(data); 
       $scope.delete = data;
     }).error(function() {
@@ -50445,7 +50445,7 @@ app.controller("itemController", function($http, $scope, $timeout, $location, $r
       let company = jQuery(e.currentTarget).find('select[name="productSelect"]').val();
       let tag = jQuery(e.currentTarget).find('input[name="editItemTag"]').val();
       console.log(company + " " + tag);
-      $http.patch(`/items/${id}`, {
+      $http.patch(`items/${id}`, {
         productId: company,
         productTag: tag
       }).success(function(data) {
@@ -50479,7 +50479,7 @@ app.controller("itemController", function($http, $scope, $timeout, $location, $r
     $scope.deleteProduct = (id) => {
       console.log(id);
 
-      $http.delete(`/items/${id}`).success(function(data) {
+      $http.delete(`items/${id}`).success(function(data) {
         jQuery('#httpMessage').removeClass('alert-danger');
         jQuery('#httpMessage').addClass('alert-success');
         jQuery('#delete').modal('hide');
