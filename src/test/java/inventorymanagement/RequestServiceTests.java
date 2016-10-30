a1d62ef87f75ddcf2d65e570c4b85bc08aa8ca30
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-30 12:46:20 +0530

    Update: Initial database, User and Request Service tests

--- src/test/java/inventorymanagement/RequestServiceTests.java
+++ src/test/java/inventorymanagement/RequestServiceTests.java
@@ -1,40 +1,48 @@
 package inventorymanagement;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import inventorymanagement.model.RequestModel;
+import inventorymanagement.service.RequestServiceInterface;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = {TestDatabaseConfig.class})
 public class RequestServiceTests {
-
+  
+  @Autowired
+  private RequestServiceInterface requestService;
+  
   @Test
-  public void test() {
-    assertTrue(2 == 2);
+  public void getAllRequestsTests() {
+    List<RequestModel> requests = requestService.getAllRequests();
+    assertEquals(requests.size(), 2);
+    
+    RequestModel request = requests.get(0);
+    assertEquals(request.getUserName(), "Manraj Singh Grover");
+    assertEquals(request.getId(), 1);
+    assertEquals(request.getProductCompany(), "OnePlus");
+    assertEquals(request.getProductName(), "Three");
+    assertEquals(request.getReply(), "YES");
   }
-
-  /*
-   * @Autowired RequestServiceInterface requestService;
-   * 
-   * @Autowired RequestServiceUtils requestServiceUtils;
-   * 
-   * @Test public void getAllRequestsTests() { List<RequestModel> requests =
-   * requestService.getAllRequests(); assertEquals(requests.size(), 4);
-   * 
-   * IncomingProductModel product = productModels.get(0); assertEquals(product.getId(), 1);
-   * assertEquals(product.getCompany(), "Apple"); assertEquals(product.getName(), "MacBook");
-   * assertEquals(product.getVersion(), "2017"); }
-   * 
-   * @Test public void getRequestByIdTests() { IncomingProductModel productModel =
-   * productService.getProductById(1);
-   * 
-   * assertEquals(productModel.getId(), 1); assertEquals(productModel.getCompany(), "Apple");
-   * assertEquals(productModel.getName(), "MacBook"); assertEquals(productModel.getVersion(),
-   * "2017"); }
-   */
+  
+  @Test
+  public void getRequestsByIdTests() {
+    RequestModel request = requestService.getRequestById(1);
+    
+    assertEquals(request.getUserName(), "Manraj Singh Grover");
+    assertEquals(request.getId(), 1);
+    assertEquals(request.getProductCompany(), "OnePlus");
+    assertEquals(request.getProductName(), "Three");
+    assertEquals(request.getReply(), "YES");
+  }
 }
--- src/test/java/inventorymanagement/UserServiceTests.java
+++ src/test/java/inventorymanagement/UserServiceTests.java
@@ -4,19 +4,27 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import inti.ws.spring.exception.client.BadRequestException;
+import inventorymanagement.model.IncomingUserModel;
 import inventorymanagement.model.UserModel;
 import inventorymanagement.service.UserServiceInterface;
 import inventorymanagement.utilities.UserServiceUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestComponent
+@Transactional
 @ContextConfiguration(classes = {TestDatabaseConfig.class})
 public class UserServiceTests {
 
@@ -27,16 +35,41 @@ public class UserServiceTests {
   UserServiceUtils userServiceUtils;
 
   @Test
+  @Rollback(true)
+  public void addUserIfExistTests() throws BadRequestException {
+    IncomingUserModel incomingUserModel = new IncomingUserModel();
+    incomingUserModel.setName("Manraj Singh Grover");
+    incomingUserModel.setEmail("manraj.singh@practo.com");
+    UserModel userModel = userService.addUserIfNotExist(incomingUserModel);
+    List<UserModel> users = userService.getAllUsers();
+    assertEquals(users.size(), 1);
+  }
+  
+  @Test
+  @Rollback(true)
+  public void addUserIfNotExistTests() throws BadRequestException {
+    IncomingUserModel incomingUserModel = new IncomingUserModel();
+    incomingUserModel.setName("Ayush Aggarwal");
+    incomingUserModel.setEmail("ayush.aggr@practo.com");
+    UserModel userModel = userService.addUserIfNotExist(incomingUserModel);
+    List<UserModel> users = userService.getAllUsers();
+
+    assertEquals(users.size(), 2);
+  }
+
+  @Test
   public void getAllUsersTests() {
     List<UserModel> users = userService.getAllUsers();
+    
+    for(UserModel user: users) {
+      System.out.println(user.getEmail());
+    }
 
     assertEquals(users.size(), 1);
 
     UserModel user = users.get(0);
     String name = user.getName();
     String email = user.getEmail();
-    System.out.println(name);
-    System.out.println(email);
 
     assertEquals(name, "Manraj Singh Grover");
     assertEquals(email, "manraj.singh@practo.com");
