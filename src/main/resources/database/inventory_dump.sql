4c278aa0ea91355e94b1e12ba02ce8c150f00a2c
Manraj Singh <manrajsinghgrover@gmail.com>
2016-11-02 15:05:22 +0530

    Update: SQL Dump updated

--- src/main/resources/database/inventory_dump.sql
+++ src/main/resources/database/inventory_dump.sql
@@ -1,33 +1,35 @@
--- MySQL dump 10.13  Distrib 5.7.16, for osx10.11 (x86_64)
---
--- Host: localhost    Database: inventory
--- ------------------------------------------------------
--- Server version	5.7.16
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.16)
+# Database: inventory
+# Generation Time: 2016-11-02 09:33:23 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `history`
---
+
+# Dump of table history
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `prod_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
   `issue_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `return_timestamp` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -41,99 +43,99 @@ CREATE TABLE `history` (
   CONSTRAINT `fk_history_item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`),
   CONSTRAINT `fk_history_prod_id` FOREIGN KEY (`prod_id`) REFERENCES `product` (`id`),
   CONSTRAINT `fk_history_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `history`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `history` WRITE;
 /*!40000 ALTER TABLE `history` DISABLE KEYS */;
-INSERT INTO `history` VALUES (1,5,1,4,'Manraj Singh Grover','2016-10-24 20:19:58',NULL),(2,5,1,1,'Manraj Singh Grover','2016-10-24 20:30:54',NULL),(3,5,1,1,'Manraj Singh Grover','2016-10-24 20:33:51',NULL),(4,5,1,2,'Manraj Singh Grover','2016-10-24 20:43:35',NULL),(5,5,2,4,'Manraj Singh Grover','2016-10-24 21:11:35',NULL),(6,5,1,5,'Manraj Singh Grover','2016-10-24 21:18:19',NULL),(7,5,1,5,'Manraj Singh Grover','2016-10-24 21:21:18',NULL),(8,5,1,5,'Manraj Singh Grover','2016-10-24 21:29:42','2016-10-24 22:21:07');
+
+INSERT INTO `history` (`id`, `user_id`, `prod_id`, `item_id`, `issue_timestamp`, `return_timestamp`)
+VALUES
+	(1,1,1,1,'2016-11-02 07:54:21',NULL);
+
 /*!40000 ALTER TABLE `history` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `item`
---
+
+# Dump of table item
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
-  `tag` varchar(50) NOT NULL,
+  `tag` varchar(30) NOT NULL,
   `create_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `available` varchar(50) NOT NULL,
+  `available` text NOT NULL,
   `modified_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `removed_timestamp` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tag` (`tag`),
   UNIQUE KEY `UKf8gk1c3u3gaam59xdm6di9ri6` (`tag`),
-  KEY `fk_item_product_id` (`product_id`),
   KEY `id` (`id`),
-  CONSTRAINT `FKd1g72rrhgq1sf7m4uwfvuhlhe` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  CONSTRAINT `fk_item_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `item`
---
+  KEY `fk_item_product_id` (`product_id`),
+  CONSTRAINT `FKd1g72rrhgq1sf7m4uwfvuhlhe` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `item` WRITE;
 /*!40000 ALTER TABLE `item` DISABLE KEYS */;
-INSERT INTO `item` VALUES (1,2,'DEFFF','2016-10-20 05:12:22','No','2016-10-25 11:40:00','2016-10-25 11:40:00'),(2,1,'PQR','2016-10-20 05:18:20','No','2016-10-24 20:43:35',NULL),(4,1,'RST','2016-10-20 05:12:22','No','2016-10-25 11:29:08',NULL),(5,2,'LMN','2016-10-20 05:12:22','Yes','2016-10-24 22:21:06',NULL);
+
+INSERT INTO `item` (`id`, `product_id`, `tag`, `create_timestamp`, `available`, `modified_timestamp`, `removed_timestamp`)
+VALUES
+	(1,1,'PQR','2016-11-01 16:27:55','No','2016-11-02 07:54:21',NULL),
+	(2,2,'XYZ','2016-11-01 16:28:08','Yes','2016-11-01 16:28:08',NULL),
+	(3,4,'LMN','2016-11-01 16:28:22','Yes','2016-11-01 17:35:34','2016-11-01 17:35:34'),
+	(4,4,'STU','2016-11-01 16:28:35','Yes','2016-11-01 17:35:34','2016-11-01 17:35:34'),
+	(5,4,'DER','2016-11-01 16:28:52','Yes','2016-11-01 17:35:34','2016-11-01 17:35:34');
+
 /*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `product`
---
+
+# Dump of table product
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `company` varchar(50) NOT NULL,
-  `version` varchar(10) NOT NULL,
+  `name` text NOT NULL,
+  `company` text NOT NULL,
+  `version` text NOT NULL,
   `create_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `removed_timestamp` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `product`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'MacBook','Applee','2016','2016-10-19 17:09:49','2016-10-26 06:29:43',NULL),(2,'Three','OnePlus','2016','2016-10-19 17:21:07','2016-10-25 12:13:40',NULL);
+
+INSERT INTO `product` (`id`, `name`, `company`, `version`, `create_timestamp`, `modified_timestamp`, `removed_timestamp`)
+VALUES
+	(1,'MacBook','Apple','2014','2016-11-01 16:26:09','2016-11-01 16:26:09',NULL),
+	(2,'iPhone','Apple','6','2016-11-01 16:26:30','2016-11-01 16:26:30',NULL),
+	(3,'iPhone','Apple','5','2016-11-01 16:26:46','2016-11-01 16:26:46',NULL),
+	(4,'One','OnePlus','2013','2016-11-01 16:27:04','2016-11-01 17:35:34','2016-11-01 17:35:34'),
+	(5,'Two','OnePlus','2014','2016-11-01 16:27:29','2016-11-01 16:27:29',NULL);
+
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `request`
---
+
+# Dump of table request
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `request`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `request` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `reply` varchar(20) NOT NULL,
+  `reply` text NOT NULL,
   `modified_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `fk_user_id` (`user_id`),
@@ -142,81 +144,73 @@ CREATE TABLE `request` (
   CONSTRAINT `FKqws2fdeknk90txm7qnm9bxd07` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `request`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `request` WRITE;
 /*!40000 ALTER TABLE `request` DISABLE KEYS */;
-INSERT INTO `request` VALUES (1,4,1,'','2016-10-20 10:35:32','2016-10-20 10:35:32',0),(2,3,1,'','2016-10-20 10:38:31','2016-10-20 10:38:31',0),(3,1,1,'','2016-10-20 11:05:52','2016-10-20 11:05:52',0),(4,2,1,'','2016-10-20 11:08:43','2016-10-20 11:08:43',0),(5,3,1,'','2016-10-20 11:10:49','2016-10-20 11:10:49',0),(6,3,1,'','2016-10-20 11:12:49','2016-10-20 11:12:49',0),(7,5,1,'Muahahahsadkjhask','2016-10-25 04:51:19','2016-10-24 07:45:15',0),(8,5,1,'Yessadasdasd','2016-10-24 14:22:32','2016-10-24 07:47:29',0),(9,5,1,'','2016-10-24 07:48:18','2016-10-24 07:48:18',0),(10,5,2,'','2016-10-24 07:48:25','2016-10-24 07:48:25',0),(11,5,1,'','2016-10-24 08:18:11','2016-10-24 08:18:11',0),(12,5,2,'','2016-10-24 12:59:52','2016-10-24 12:59:52',0),(13,5,2,'','2016-10-24 16:09:54','2016-10-24 16:09:54',0),(14,5,1,'','2016-10-24 16:09:58','2016-10-24 16:09:58',0),(15,5,2,'','2016-10-25 11:46:33','2016-10-25 11:46:33',0);
+
+INSERT INTO `request` (`id`, `user_id`, `product_id`, `reply`, `modified_timestamp`, `created_timestamp`)
+VALUES
+	(1,1,1,'Replyingtohelo','2016-11-01 20:25:35','2016-11-01 19:27:44');
+
 /*!40000 ALTER TABLE `request` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `role`
---
+
+# Dump of table role
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
+  `name` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'admin'),(2,'user');
+
+INSERT INTO `role` (`id`, `name`)
+VALUES
+	(1,'admin'),
+	(2,'user');
+
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `user`
---
+
+# Dump of table user
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `contact` varchar(20) NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
   `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `removed_timestamp` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Pradesh Khanna','pradeshkhanna@gmail.com','7654321098','2016-10-18 11:38:10','2016-10-20 09:25:51','2016-10-20 09:25:51'),(2,'Bhuv','yo@yo.com','9898989898','2016-10-18 22:01:24','2016-10-20 09:55:44','2016-10-20 09:26:41'),(3,'Bhuv','yo@yo.com','9898989898','2016-10-19 06:06:19','2016-10-20 09:56:21','2016-10-20 09:56:21'),(4,'Manraj Singh','manraj@gmail.com','9811040427','2016-10-19 09:42:55','2016-10-19 09:42:55',NULL),(5,'Manraj Singh Grover','manraj.singh@practo.com','9898989898','2016-10-22 09:54:09','2016-10-22 09:54:09',NULL);
+
+INSERT INTO `user` (`id`, `name`, `email`, `created_timestamp`)
+VALUES
+	(1,'Manraj Singh Grover','manraj.singh@practo.com','2016-11-01 16:24:07');
+
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `user_role`
---
+
+# Dump of table user_role
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `user_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `user_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -229,26 +223,23 @@ CREATE TABLE `user_role` (
   CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
   CONSTRAINT `fk_user_roles_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_user_roles_roles_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_role`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (1,5,1);
+
+INSERT INTO `user_role` (`id`, `user_id`, `role_id`)
+VALUES
+	(1,1,1);
+
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-10-26 12:53:56
